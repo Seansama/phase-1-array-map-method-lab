c9586c11a3ae16eb7161b1f47883922b81cd0432
ShaunM <seansama__@pop-os.localdomain>
2022-08-26 11:54:06 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -10,7 +10,28 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
+  const newTutorials = [];
+  for (const a of tutorials){
+    const capturedName = a;
+    const splittedArray = capturedName.split(' ');
+    let newMessage = '';
+    splittedArray.map(word => {
+      let wordArray = [];
+      let text = '';
+      for (const a of word){
+        wordArray.push(a);
+      }
+      wordArray[0] = wordArray[0].toUpperCase();
+      for (const b of wordArray){
+        text += b;
+      }
+      newMessage += `${text} `;
+      return newMessage;
+    })
+    newTutorials.push(newMessage.substring(0, newMessage.length - 1));
+  }
+  return newTutorials;
 }
+
+console.log(titleCased())
